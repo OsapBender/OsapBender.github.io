06d0d79479d28c3020cd8aa83cc1dfc1ce04bdd2
OsapBender <43818081+OsapBender@users.noreply.github.com>
2021-06-23 15:04:26 +0400

    Add files via upload

---
+++ precache-manifest.fe6915ab9ffaba4a8fe59d7dfe7d9f25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f16f6d5e1acd02f28ffa7aa2d55136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfdf0ae4c7e9b8415398",
+    "url": "/static/css/main.8f658cbc.chunk.css"
+  },
+  {
+    "revision": "dd659d27e1d21728a7e6",
+    "url": "/static/js/2.a0cb31e1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a0cb31e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdf0ae4c7e9b8415398",
+    "url": "/static/js/main.7fb5c8dd.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2b5d8441632c6b57bbd5968c4c335df2",
+    "url": "/static/media/logo_t_one.2b5d8441.svg"
+  },
+  {
+    "revision": "bb14a2b8354976b26cadd913e4ad37d3",
+    "url": "/static/media/me.bb14a2b8.jpg"
+  }
+]);
